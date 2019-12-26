4406771b224257a84069763ac99b2b2f7eef18cb
Daryll  Osis <dosis@csumb.edu>
2019-12-26 00:44:04 -0800

    added a front end with react, and created some folders inside client to keep everything organized

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+
+import Navbar from './components/layout/Navbar';
+
+import './App.css';
+
+const App = () => (
+  <Fragment>
+    <h1>app</h1>
+    <Navbar />
+  </Fragment>
+);
+
+export default App;
