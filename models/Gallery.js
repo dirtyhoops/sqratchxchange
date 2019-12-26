971e92b2b90b1f5af1acb1482a7d2acf73f71171
Daryll  Osis <dosis@csumb.edu>
2019-12-25 23:29:22 -0800

    added an api routes for adding/deleting/getting items. user authentication to be added

---
+++ models/Gallery.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const GallerySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String
+  },
+  image: {
+    type: String,
+    required: true
+  },
+  orientation: {
+    type: String,
+    required: true
+  },
+  dateAdded: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = Gallery = mongoose.model('gallery', GallerySchema);
---
+++ routes/api/gallery.js
@@ -0,0 +1,108 @@
+const express = require('express');
+const router = express.Router();
+const { check, validationResult } = require('express-validator');
+
+const Gallery = require('../../models/Gallery');
+
+// @route     GET api/gallery
+// @desc      Gallery route
+// @access    Public
+router.get('/', async (req, res) => {
+  try {
+    const items = await Gallery.find().sort({ date: -1 });
+    res.json(items);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route     GET post/gallery
+// @desc      Add an item to the gallery
+// @access    Public
+router.post(
+  '/',
+  [
+    check('name', 'Name is required')
+      .not()
+      .isEmpty(),
+    check('description', 'Description is required')
+      .not()
+      .isEmpty(),
+    check('image', 'Image link is required')
+      .not()
+      .isEmpty(),
+    check('orientation', 'Orientation is required')
+      .not()
+      .isEmpty()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const newItem = new Gallery({
+        name: req.body.name,
+        description: req.body.description,
+        image: req.body.image,
+        orientation: req.body.orientation
+      });
+
+      const post = await newItem.save();
+
+      res.json(post);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
+
+// @route     GET api/gallery/:id
+// @desc      Get item by ID
+// @access    Public
+router.get('/:id', async (req, res) => {
+  try {
+    const item = await Gallery.findById(req.params.id);
+
+    if (!item) {
+      return res.status(404).json({ msg: 'Item not found' });
+    }
+
+    res.json(item);
+  } catch (err) {
+    console.error(err.message);
+
+    if (err.kind === 'ObjectId') {
+      return res.status(404).json({ msg: 'Item not found' });
+    }
+
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route     DELETE api/gallery
+// @desc      Delete an item
+// @access    Public
+router.delete('/:id', async (req, res) => {
+  try {
+    const item = await Gallery.findById(req.params.id);
+
+    // Have a login later and make sure that we check if the admin is logged in before deleting an item
+    await item.remove();
+
+    res.json({ msg: 'Item is removed' });
+  } catch (err) {
+    console.error(err.message);
+
+    if (err.kind === 'ObjectId') {
+      return res.status(404).json({ msg: 'Item not found' });
+    }
+
+    res.status(500).send('Server Error');
+  }
+});
+
+module.exports = router;
