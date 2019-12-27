6c745bb9f3656ced81e14d4e45003401a71d86c2
Daryll  Osis <dosis@csumb.edu>
2019-12-27 15:30:05 -0800

    added some shapes and made the about section image aesthetically pleasing.

--- client/src/components/pages/home/About.js
+++ client/src/components/pages/home/About.js
@@ -6,25 +6,25 @@ const About = () => {
       <div className='about container'>
         <div className='about__container'>
           <div className='about__container--left'>
-            <div className='about__images'></div>
-            <div className='about__images__border'>
-              <div className='about__images1'></div>
+            <div className='about__image--3'></div>
+            <div className='about__image--4'></div>
+            <div className='about__image--1'></div>
+            <div className='about__image--2__border'>
+              <div className='about__image--2'></div>
             </div>
           </div>
           <div className='about__container--right'>
-            <h2 className='about__text--heading'>About Sqratch Xchange</h2>
-            <p className='about__text--sub'>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-              Consequatur voluptas cupiditate aliquid nesciunt earum! Mollitia
-              voluptatem consequatur doloremque quae doloribus eum provident,
-              dolorum sapiente sint cumque quod aliquid dolores totam!
-            </p>
-            <p className='about__text--sub'>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-              Consequatur voluptas cupiditate aliquid nesciunt earum! Mollitia
-              voluptatem consequatur doloremque quae doloribus eum provident,
-              dolorum sapiente sint cumque quod aliquid dolores totam!
-            </p>
+            <div className='about__text'>
+              <h2 className='heading-primary'>About Sqratch Xchange</h2>
+              <p className='text--about'>
+                Sqratch Xchange was established September 1, 2019 by Allen
+                Ganaden and Abrena Ganaden. We are Monterey's neighborhood
+                premier vintage boutique founded on the principle of bringing
+                our customers the best possible deals on new and vintage
+                streetwear from the 80s to the 200s. We want you to look good at
+                an affordable price.
+              </p>
+            </div>
           </div>
         </div>
       </div>
--- client/src/components/pages/home/Offer.js
+++ client/src/components/pages/home/Offer.js
@@ -5,7 +5,16 @@ const Offer = () => {
     <section className='section-offer'>
       <div className='offer__topshape'></div>
       <div className='offer container'>
-        <h2>aaaaaaaaaa</h2>
+        <h2 className='heading-primary'>what we offer</h2>
+        <div className='offer__container'>
+          {/* CHANGE THIS TO REACT WHERE IT LOOPS THROUGH ITEMS SO I DONT HAVE TO DO A LOT OF DIVS MANUALLY */}
+          <div className='offer__grids'></div>
+          <div className='offer__grids'></div>
+          <div className='offer__grids'></div>
+          <div className='offer__grids'></div>
+          <div className='offer__grids'></div>
+          <div className='offer__grids'></div>
+        </div>
       </div>
     </section>
   );
